9dc946f44df3fb4b42405db04c020cd071b355b1
PSriGanesh <sriganeshpolisetty@gmail.com>
2023-03-16 17:42:10 +0530

    Refactor using classes

--- script.js
+++ script.js
@@ -4,6 +4,11 @@ let form = document.querySelector('.book-form');
 let index=0;
 let array = [];
 let inputs = document.querySelectorAll('body form div input');
+let pageField = form.children[5].children[0]
+pageField.addEventListener('keydown',(e)=>{
+    if(e.key=='e')
+        e.preventDefault();
+})
 inputs.forEach((input)=> {
     if(input.id=='bcheck')
         return;
@@ -43,69 +48,71 @@ function createBook(event) {
     let book = new Book(name,author,pages,stats);
     event.preventDefault();
 }
-function Book(name,author,pages,stats) {
-    this.bname = name;
-    this.author = author;
-    this.pages=pages;
-    this.id = index;
-    index++;
-    array.push(this);
-    let ele = document.createElement('div');
-    let tempdiv1 = document.createElement('div');
-    let tempdiv2 = document.createElement('div');
-    let tempdiv3 = document.createElement('div');
-    let tempdiv4 = document.createElement('div');
-    let remove = document.createElement('div');
-    ele.dataset.ind=this.id;
-    remove.addEventListener('click',this.removeBook);
-    remove.textContent="Remove Book";
-    remove.classList.add('removebtn');
-    tempdiv1.textContent = `${this.bname}`;
-    ele.appendChild(tempdiv1);
-    tempdiv2.textContent = `By ${this.author}`;
-    ele.appendChild(tempdiv2);
-    tempdiv3.textContent = `Pages - ${this.pages}`;
-    ele.appendChild(tempdiv3);
-    tempdiv4.addEventListener('click',this.setStatus);
-    tempdiv4.classList.add('statusbtn');
-    if(stats==true)
-    this.stats="Read";
-    else
-    this.stats="Not Read";
-    tempdiv4.textContent = `${this.stats}`;
-    ele.appendChild(tempdiv4);
-    ele.appendChild(remove);
-    ele.classList.add('book-card');
-    content.appendChild(ele);
-    overlay.style.display='none';
-    form.classList.toggle('active');
-}
 
-Book.prototype.setStatus = function() {
-    let tempindex = this.parentElement.dataset.ind;
-    let selection = document.querySelector(`[data-ind="${tempindex}"]`).children[3];
-    let i=0;
-    for(i=0;i<array.length;i++) {
-        if(array[i].id==tempindex)
-            break;
+class Book {
+    constructor(name,author,pages,stats){
+        this.bname = name;
+        this.author = author;
+        this.pages=pages;
+        this.id = index;
+        index++;
+        array.push(this);
+        let ele = document.createElement('div');
+        let tempdiv1 = document.createElement('div');
+        let tempdiv2 = document.createElement('div');
+        let tempdiv3 = document.createElement('div');
+        let tempdiv4 = document.createElement('div');
+        let remove = document.createElement('div');
+        ele.dataset.ind=this.id;
+        remove.addEventListener('click',this.removeBook);
+        remove.textContent="Remove Book";
+        remove.classList.add('removebtn');
+        tempdiv1.textContent = `${this.bname}`;
+        ele.appendChild(tempdiv1);
+        tempdiv2.textContent = `By ${this.author}`;
+        ele.appendChild(tempdiv2);
+        tempdiv3.textContent = `Pages - ${this.pages}`;
+        ele.appendChild(tempdiv3);
+        tempdiv4.addEventListener('click',this.setStatus);
+        tempdiv4.classList.add('statusbtn');
+        if(stats==true)
+        this.stats="Read";
+        else
+        this.stats="Not Read";
+        tempdiv4.textContent = `${this.stats}`;
+        ele.appendChild(tempdiv4);
+        ele.appendChild(remove);
+        ele.classList.add('book-card');
+        content.appendChild(ele);
+        overlay.style.display='none';
+        form.classList.toggle('active');
     }
-    if(selection.textContent=="Not Read"){
-        selection.textContent="Read";
-        array[i].stats="Read";
+    setStatus(){
+        let tempindex = this.parentElement.dataset.ind;
+        let selection = document.querySelector(`[data-ind="${tempindex}"]`).children[3];
+        let i=0;
+        for(i=0;i<array.length;i++) {
+            if(array[i].id==tempindex)
+                break;
+        }
+        if(selection.textContent=="Not Read"){
+            selection.textContent="Read";
+            array[i].stats="Read";
+        }
+        else if(selection.textContent=="Read"){
+            selection.textContent="Not Read";
+            array[i].stats="Not Read";
+        }
     }
-    else if(selection.textContent=="Read"){
-        selection.textContent="Not Read";
-        array[i].stats="Not Read";
+    removeBook(){
+        let tempindex = this.parentElement.dataset.ind;
+        let i=0;
+        for(i=0;i<array.length;i++) {
+            if(array[i].id==tempindex)
+                break;
+        }
+        array.splice(i,1);
+        let selection = document.querySelector(`[data-ind="${tempindex}"]`);
+        selection.remove();
     }
-}
-Book.prototype.removeBook = function() {
-    let tempindex = this.parentElement.dataset.ind;
-    let i=0;
-    for(i=0;i<array.length;i++) {
-        if(array[i].id==tempindex)
-            break;
-    }
-    array.splice(i,1);
-    let selection = document.querySelector(`[data-ind="${tempindex}"]`);
-    selection.remove();
 }
